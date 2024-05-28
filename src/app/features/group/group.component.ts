6b280e281e93e7f263e4bc5e55ddf373f4b71c4b
Lucas Wang <lucaschiwang@gmail.com>
2024-05-28 12:13:07 +1000

    Updated UI.

--- src/app/features/group/group.component.ts
+++ src/app/features/group/group.component.ts
@@ -1,15 +1,24 @@
+import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
 import { ChatService } from '../../core/services/chat.service';
+import { statusFeature } from '../../core/store/status/status.reducer';
 
 @Component({
   selector: 'app-group',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgClass],
   templateUrl: './group.component.html',
   styleUrl: './group.component.scss',
 })
 export class GroupComponent {
+  store = inject(Store);
   chatService = inject(ChatService);
+
+  isSelected$ = this.store
+    .select(statusFeature.selectActivatedGroup)
+    .pipe(map((group) => group?._id == this.groupId));
 
   @Input()
   groupName!: string;
--- src/app/features/room/room.component.ts
+++ src/app/features/room/room.component.ts
@@ -1,15 +1,24 @@
+import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
 import { ChatService } from '../../core/services/chat.service';
+import { statusFeature } from '../../core/store/status/status.reducer';
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgClass],
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss',
 })
 export class RoomComponent {
+  store = inject(Store);
   chatService = inject(ChatService);
+
+  isSelected$ = this.store
+    .select(statusFeature.selectJoinedRoom)
+    .pipe(map((roomId) => roomId == this.roomId));
 
   @Input()
   roomName!: string;
